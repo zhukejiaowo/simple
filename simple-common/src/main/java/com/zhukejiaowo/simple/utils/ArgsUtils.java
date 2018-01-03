5785c15e61cc2d0cc3e5641d1dad621e6aa26d06
caishang9 <caishang@jd.com>
2018-01-03 21:02:44 +0800

    fixed:增加args的处理帮助类.

--- simple-common/src/main/java/com/zhukejiaowo/simple/utils/ArgsUtils.java
+++ simple-common/src/main/java/com/zhukejiaowo/simple/utils/ArgsUtils.java
@@ -1,5 +1,11 @@
 package com.zhukejiaowo.simple.utils;
 
+
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Collection;
+
 /**
  * Description:
  *
@@ -13,4 +19,87 @@ package com.zhukejiaowo.simple.utils;
  * =========================== 维护日志 ===========================
  */
 public class ArgsUtils {
+
+    public static void check(final boolean expression, final String message) {
+        if (!expression) {
+            throw new IllegalArgumentException(message);
+        }
+    }
+
+    public static void check(final boolean expression, final String message, final Object... args) {
+        if (!expression) {
+            throw new IllegalArgumentException(String.format(message, args));
+        }
+    }
+
+    public static void check(final boolean expression, final String message, final Object arg) {
+        if (!expression) {
+            throw new IllegalArgumentException(String.format(message, arg));
+        }
+    }
+
+    public static <T> T notNull(final T argument, final String name) {
+        if (argument == null) {
+            throw new IllegalArgumentException(name + " may not be null");
+        }
+        return argument;
+    }
+
+    public static <T extends CharSequence> T notEmpty(final T argument, final String name) {
+        if (argument == null) {
+            throw new IllegalArgumentException(name + " may not be null");
+        }
+        if (StringUtils.isEmpty(argument)) {
+            throw new IllegalArgumentException(name + " may not be empty");
+        }
+        return argument;
+    }
+
+    public static <T extends CharSequence> T notBlank(final T argument, final String name) {
+        if (argument == null) {
+            throw new IllegalArgumentException(name + " may not be null");
+        }
+        if (StringUtils.isBlank(argument)) {
+            throw new IllegalArgumentException(name + " may not be blank");
+        }
+        return argument;
+    }
+
+    public static <E, T extends Collection<E>> T notEmpty(final T argument, final String name) {
+        if (argument == null) {
+            throw new IllegalArgumentException(name + " may not be null");
+        }
+        if (argument.isEmpty()) {
+            throw new IllegalArgumentException(name + " may not be empty");
+        }
+        return argument;
+    }
+
+    public static int positive(final int n, final String name) {
+        if (n <= 0) {
+            throw new IllegalArgumentException(name + " may not be negative or zero");
+        }
+        return n;
+    }
+
+    public static long positive(final long n, final String name) {
+        if (n <= 0) {
+            throw new IllegalArgumentException(name + " may not be negative or zero");
+        }
+        return n;
+    }
+
+    public static int notNegative(final int n, final String name) {
+        if (n < 0) {
+            throw new IllegalArgumentException(name + " may not be negative");
+        }
+        return n;
+    }
+
+    public static long notNegative(final long n, final String name) {
+        if (n < 0) {
+            throw new IllegalArgumentException(name + " may not be negative");
+        }
+        return n;
+    }
 }
