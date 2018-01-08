1eec2affcf2a9f71ed9dc40188ae49dfbf01543a
caishang9 <caishang9>
2018-01-08 18:11:27 +0800

    fixed:httpclient 配置.

--- simple-common/src/test/java/com/zhukejiaowo/simple/httpclient/ClientConfiguration.java
+++ simple-common/src/test/java/com/zhukejiaowo/simple/httpclient/ClientConfiguration.java
@@ -58,6 +58,7 @@ public class ClientConfiguration {
 
         // Use custom message parser / writer to customize the way HTTP
         // messages are parsed from and written out to the data stream.
+        //
         HttpMessageParserFactory<HttpResponse> responseParserFactory = new DefaultHttpResponseParserFactory() {
 
             @Override
@@ -94,6 +95,7 @@ public class ClientConfiguration {
         // initialization of outgoing HTTP connections. Beside standard connection
         // configuration parameters HTTP connection factory can define message
         // parser / writer routines to be employed by individual connections.
+        //使用自定义连接工厂来定制流程发送HTTP连接的初始化。除了标准的连接配置参数HTTP连接工厂可以定义消息解析器/编写程序由单个连接使用。
         HttpConnectionFactory<HttpRoute, ManagedHttpClientConnection> connFactory = new ManagedHttpClientConnectionFactory(
                 requestWriterFactory, responseParserFactory);
 
@@ -101,9 +103,11 @@ public class ClientConfiguration {
         // an arbitrary network socket. The process of network socket initialization,
         // its connection to a remote address and binding to a local one is controlled
         // by a connection socket factory.
+        //客户机HTTP连接对象在完全初始化时可以绑定到任意网络套接字。网络套接字初始化的过程，它与远程地址的连接和绑定到本地的地址是由连接套接字工厂控制的。
 
         // SSL context for secure connections can be created either based on
         // system or application specific properties.
+        //可以基于系统或应用程序特定属性创建安全连接的SSL上下文。
         SSLContext sslcontext = SSLContexts.createSystemDefault();
 
         // Create a registry of custom connection socket factories for supported
@@ -114,6 +118,7 @@ public class ClientConfiguration {
                 .build();
 
         // Use custom DNS resolver to override the system DNS resolution.
+        //使用自定义DNS解析器来覆盖系统DNS解析。
         DnsResolver dnsResolver = new SystemDefaultDnsResolver() {
 
             @Override
@@ -128,26 +133,34 @@ public class ClientConfiguration {
         };
 
         // Create a connection manager with custom configuration.
+
+        //******************使用自定义配置创建连接管理器。
         PoolingHttpClientConnectionManager connManager = new PoolingHttpClientConnectionManager(
                 socketFactoryRegistry, connFactory, dnsResolver);
 
         // Create socket configuration
+        //创建套接字配置
         SocketConfig socketConfig = SocketConfig.custom()
                 .setTcpNoDelay(true)
                 .build();
         // Configure the connection manager to use socket configuration either
         // by default or for a specific host.
+        //配置连接管理器以使用套接字配置
+        //默认情况下或特定主机。
         connManager.setDefaultSocketConfig(socketConfig);
         connManager.setSocketConfig(new HttpHost("somehost", 80), socketConfig);
         // Validate connections after 1 sec of inactivity
+        //在1秒的不活动后验证连接
         connManager.setValidateAfterInactivity(1000);
 
         // Create message constraints
+        //创建消息的约束
         MessageConstraints messageConstraints = MessageConstraints.custom()
                 .setMaxHeaderCount(200)
                 .setMaxLineLength(2000)
                 .build();
         // Create connection configuration
+        //创建连接配置
         ConnectionConfig connectionConfig = ConnectionConfig.custom()
                 .setMalformedInputAction(CodingErrorAction.IGNORE)
                 .setUnmappableInputAction(CodingErrorAction.IGNORE)
@@ -156,20 +169,27 @@ public class ClientConfiguration {
                 .build();
         // Configure the connection manager to use connection configuration either
         // by default or for a specific host.
+        //配置连接管理器以使用连接配置
+        //默认情况下或特定主机。
         connManager.setDefaultConnectionConfig(connectionConfig);
         connManager.setConnectionConfig(new HttpHost("somehost", 80), ConnectionConfig.DEFAULT);
 
         // Configure total max or per route limits for persistent connections
         // that can be kept in the pool or leased by the connection manager.
+        //为持久连接配置总最大值或路径限制
+        //可以保存在池中，也可以由连接管理器租用。
         connManager.setMaxTotal(100);
         connManager.setDefaultMaxPerRoute(10);
         connManager.setMaxPerRoute(new HttpRoute(new HttpHost("somehost", 80)), 20);
 
         // Use custom cookie store if necessary.
+        //在必要的时候使用定制的cookie。
         CookieStore cookieStore = new BasicCookieStore();
         // Use custom credentials provider if necessary.
+        //在必要时使用自定义凭证。
         CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
         // Create global request configuration
+        //创建全局请求配置.
         RequestConfig defaultRequestConfig = RequestConfig.custom()
                 .setCookieSpec(CookieSpecs.DEFAULT)
                 .setExpectContinueEnabled(true)
@@ -178,6 +198,7 @@ public class ClientConfiguration {
                 .build();
 
         // Create an HttpClient with the given custom dependencies and configuration.
+        //创建一个具有给定自定义依赖关系和配置的HttpClient。
         CloseableHttpClient httpclient = HttpClients.custom()
                 .setConnectionManager(connManager)
                 .setDefaultCookieStore(cookieStore)
@@ -190,6 +211,7 @@ public class ClientConfiguration {
             HttpGet httpget = new HttpGet("http://httpbin.org/get");
             // Request configuration can be overridden at the request level.
             // They will take precedence over the one set at the client level.
+            //请求配置可以在请求级别覆盖。它们将优先于客户端的一个集合。
             RequestConfig requestConfig = RequestConfig.copy(defaultRequestConfig)
                     .setSocketTimeout(5000)
                     .setConnectTimeout(5000)
@@ -199,9 +221,12 @@ public class ClientConfiguration {
             httpget.setConfig(requestConfig);
 
             // Execution context can be customized locally.
+            //它们将优先于客户端的一个集合。
             HttpClientContext context = HttpClientContext.create();
             // Contextual attributes set the local context level will take
             // precedence over those set at the client level.
+            //上下文属性设置了本地上下文级别.
+            //优先于客户端的设置。
             context.setCookieStore(cookieStore);
             context.setCredentialsProvider(credentialsProvider);
 
@@ -216,10 +241,12 @@ public class ClientConfiguration {
                 // Once the request has been executed the local context can
                 // be used to examine updated state and various objects affected
                 // by the request execution.
-
+               // 一旦请求被执行，本地上下文可以用于检查受影响的更新状态和各种对象请求执行。
                 // Last executed request
+                //最后一次执行请求
                 context.getRequest();
                 // Execution route
+                //执行路线
                 context.getHttpRoute();
                 // Target auth state
                 context.getTargetAuthState();
